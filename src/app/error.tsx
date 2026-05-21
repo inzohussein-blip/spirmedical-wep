67c56e69a1c9034c6750bf4715d225e5efe9e30e
hussein <inzohussein@gmail.com>
2026-05-21 17:30:39 +0300

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -11,6 +11,7 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
+    // eslint-disable-next-line no-console
     console.error('App Error:', error);
   }, [error]);
 
