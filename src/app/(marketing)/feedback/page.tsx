f945b9bf7923ce1c20afd3c7a0b10eeb4e0b5ccc
hussein <inzohussein@gmail.com>
2026-05-23 12:17:21 +0300

    Create page.tsx

---
+++ src/app/(marketing)/feedback/page.tsx
@@ -0,0 +1,10 @@
+import FeedbackClient from './FeedbackClient';
+
+export const metadata = {
+  title: 'شاركنا رأيك · Spir Medical',
+  description: 'ساعدنا في تطوير الخدمة - شاركنا اقتراحاتك وملاحظاتك',
+};
+
+export default function FeedbackPage() {
+  return <FeedbackClient />;
+}
